4e3765d91ba68674550c16706c6c0315c014e5c7
Universalmike <150071045+Universalmike@users.noreply.github.com>
2025-05-13 23:10:24 +0100

    Update price trend.py

--- price trend.py
+++ price trend.py
@@ -85,9 +85,9 @@ def classify_leaf(image_file):
     predicted_index = np.argmax(predicted_probs)
     predicted_label = class_labels[predicted_index]
     #predicted_label = class_labels[np.argmax(predicted_probs)]
-    confidence = predicted_probs[predicted_index]
+    #confidence = predicted_probs[predicted_index]
 
-    return f"{predicted_label} (Confidence: {confidence:.2f})"
+    return predicted_label #f"{predicted_label} (Confidence: {confidence:.2f})"
 
 
 st.title("Agri Forecast & Leaf Classifier App")
