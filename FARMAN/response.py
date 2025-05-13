d17b8d5a7e6fd7e5b20085e1cc13ca01533441e5
Universalmike <150071045+Universalmike@users.noreply.github.com>
2025-05-13 04:11:07 +0100

    Update response.py

--- FARMAN/response.py
+++ FARMAN/response.py
@@ -1,3 +1,8 @@
+import google.generativeai as genai
+
+api_key = 'AIzaSyDg9kb3rOzT05MhCU32rJDOGdz2eXTTp9c'
+genai.configure(api_key=api_key)
+
 def generate_response(query, relevant_documents):
     context = "\n".join([doc.page_content for doc in relevant_documents])  # Combine the relevant documents into context
     prompt = f"You are an experienced farmer, Given the following information \n{context}\nAnswer the following question as an experienced farmer:\n{query}, also dont mention, based on the text, just answer me like we are in a conversation"
