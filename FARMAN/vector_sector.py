a98d65ab0541ba3fefe1123382f516295c98d5f8
Universalmike <150071045+Universalmike@users.noreply.github.com>
2025-05-13 19:55:32 +0100

    Update vector_sector.py

--- FARMAN/vector_sector.py
+++ FARMAN/vector_sector.py
@@ -2,7 +2,7 @@ from langchain.vectorstores import FAISS
 from langchain.embeddings import HuggingFaceEmbeddings
 from langchain.vectorstores import Chroma
 
-def create_vector_store(docs):
+def create_vector_store(chunks):
     # Use an efficient embedding model
     embedding_model = HuggingFaceEmbeddings(model_name="sentence-transformers/all-MiniLM-L6-v2")
     vector_db = Chroma.from_documents(chunks, embedding_model, persist_directory="./chroma_db_hf")
