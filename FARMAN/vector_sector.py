4931d03814d4fe5b975f78937fd556084ef2168a
Universalmike <150071045+Universalmike@users.noreply.github.com>
2025-05-13 05:36:02 +0100

    Update vector_sector.py

--- FARMAN/vector_sector.py
+++ FARMAN/vector_sector.py
@@ -4,6 +4,6 @@ from langchain.embeddings import HuggingFaceEmbeddings
 def create_vector_store(docs):
     # Use an efficient embedding model
     embedding_model = HuggingFaceEmbeddings(model_name="sentence-transformers/all-MiniLM-L6-v2")
-    vector_db = Chroma.from_documents(split_docs, embedding_model, persist_directory="/agrivision/FARMAN")
+    vector_db = Chroma.from_documents(split_docs, embedding_model, persist_directory="./chroma_db_hf")
     vector_db.persist()
     return vector_db
